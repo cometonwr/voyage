eb899c225fb2daf92d29b7cb80c556330fb184aa
cometonwr <60749423+cometonwr@users.noreply.github.com>
2020-02-17 12:41:12 +0900

    0217 수업
    수업이다

--- tutorial/Assets/Recipe.cs
+++ tutorial/Assets/Recipe.cs
@@ -8,18 +8,13 @@ using UnityEngine;
 [Serializable]
 public class Recipe
 {
-    public int Row;
-    public int Column;
-
     public BlockType[] Blocks;
-
-
 }
 
 public class Test
 {
-    int localX = 10;
-    int localY = 20;
+    const int localX = 10;
+    const int localY = 20;
 
     BlockType[,] blockTypes = new BlockType[localX, localY];
 
